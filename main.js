f5b197924e27e2e81da6eb42aa5e73dfab87646f
khanshy <shaheryarkhan1008@gmail.com>
2021-08-21 20:49:49 +0500

    fix min width/height of the app

--- main.js
+++ main.js
@@ -17,8 +17,9 @@ function createWindow () {
 
     // Create the browser window.
     main = new BrowserWindow({
-        width: 800,
         height: 600,
+        minWidth: 800,
+        minHeight: 520,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         },
